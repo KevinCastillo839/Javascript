1a70ab5ef96a6c0aeb72882005ea12a83074267b
KevinCastillo839 <kevinandres839@gmail.com>
2025-08-26 18:46:54 -0600

    first commit

---
+++ Basic/00-helloworld.js
@@ -0,0 +1,24 @@
+console.log("Hello, World!")
+
+//este es un comentario de una sola línea
+
+/*
+este es un comentario  
+
+*/
+console.log("5")//número entre comillas es un string
+console.log(5)//número sin comillas es un number
+console.log(5 + 3)//suma
+console.log(5 - 3)//resta   
+console.log(5 * 3)//multiplicación
+console.log(5 / 3)//división
+console.log(5 % 3)//módulo o residuo
+console.log(5 ** 3)//potencia
+console.log(5 + 3 * 2)//precedencia de operadores
+console.log((5 + 3) * 2)//uso de paréntesis para cambiar la precedencia
+console.log("5" + "3")//concatenación de strings
+console.log("5" + 3)//concatenación de string y number
+console.log(5 + "3")//concatenación de number y string
+console.log("5" - 3)//resta de string y number (el string se convierte a number)
+console.log(5 - "3")//resta de number y string (el string se convierte a number)
+console.log("5" * 3)//multiplicación de string y number (el string
